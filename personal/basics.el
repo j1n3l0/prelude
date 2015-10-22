c9b2a2b1c5f91acab92c68e6a1b04480dcaf758a
Nelo Onyiah <nelo.onyiah@net-a-porter.com>
2015-10-22 15:05:27 +0100

    Add some basic configuration
    Note that helm is not fully configured yet (I don't think).

---
+++ personal/basics.el
@@ -0,0 +1,18 @@
+(require 'golden-ratio)
+
+(golden-ratio-mode 1)
+(exec-path-from-shell-initialize)
+
+;;
+;; associate file to major modes
+(add-to-list 'auto-mode-alist '("\\.pl\\'" . cperl-mode))
+(add-to-list 'auto-mode-alist '("\\.pm\\'" . cperl-mode))
+(add-to-list 'auto-mode-alist '("\\.t\\'"  . cperl-mode))
+
+;;
+;; erc - set buffer name
+(setq erc-prompt (lambda () (concat "[" (buffer-name) "]")))
+
+;;
+;; helm
+(require 'helm-config)
